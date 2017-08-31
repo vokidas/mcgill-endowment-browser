88191246a1452cfb7bf63a1ac0e702f24f2e68d6
Igor Sadikov <igor@vokidas.com>
2017-08-31 16:23:03 -0400

    created react app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.set('port', process.env.PORT || 3001)
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+app.get('/api/holdings', (req, res) => {
+  res.json([])
+})
+
+app.listen(app.get('port'), () => {
+  console.info('Listening on http://localhost:' + app.get('port')) // eslint-disable-line no-console
+})
