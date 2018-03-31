0d7cde6093c816a8be1f830c390f177400f86502
Igor Sadikov <igorsadikov@gmail.com>
2018-03-31 16:37:33 -0400

    splash page text correction

--- client/src/Splash.js
+++ client/src/Splash.js
@@ -122,7 +122,7 @@ function Splash (props) {
   return (
     <Content>
       <p>The <strong className="secondary-font">mcgill endowment browser</strong> is a tool to explore McGill University's investment data, obtained by Divest McGill via Access to Information requests.</p>
-      <p>We have organized the endowment into several categories of interest, putting into focusing the investments that we do not consider socially and environmentally responsible. Contextual information about each company and links to further research are provided where available.</p>
+      <p>We have organized the endowment into several categories of interest, bringing into focus the investments that we do not consider socially and environmentally responsible. Contextual information about each company and links to further research are provided where available.</p>
       <SplashContent {...props} />
     </Content>
   )
